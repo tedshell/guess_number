fe8428b9668193ae4dff24be9f7792ee8a2f2b09
鄒昀 <tedshell123456@gmail.com>
2024-03-12 13:07:34 +0900

    add count

--- guess_number.py
+++ guess_number.py
@@ -1,15 +1,19 @@
 import random
 
 r = random.randint(1, 100)
+count = 0
 while True:
+	count += 1 #count = count + 1
 	ans = input('請輸入數字：')
 	ans = int(ans)
 	if r == ans:
 		print('正確')
+		print('你猜了', count, '次')
 		break
 	else:
 		if r < ans:
-			print('比答案大，請重新輸入')
+			print('比答案大')
 		else:
-			print('比答案小，請重新輸入')	
+			print('比答案小')	
+	print('你猜了', count, '次')
     
