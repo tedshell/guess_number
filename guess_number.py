8bf53e9c0d560b11ef05aa16340f6f5575aa2ba9
鄒昀 <tedshell123456@gmail.com>
2024-03-12 13:16:55 +0900

    add ver

--- guess_number.py
+++ guess_number.py
@@ -1,6 +1,10 @@
 import random
+start = input('請輸入範圍開始值')
+end = input('請輸入範圍結束值')
+s = int(start)
+e = int(end)
 
-r = random.randint(1, 100)
+r = random.randint(s, e)
 count = 0
 while True:
 	count += 1 #count = count + 1
